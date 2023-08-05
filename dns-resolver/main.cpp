e2f747cbba0977e484f055efddd67a7ca0a207fe
IsaErtunga <isa.ertunga@gmail.com>
2023-08-05 19:24:07 +0200

    Initial Commit

---
+++ dns-resolver/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  dns-resolver
+//
+//  Created by Isa Ertunga on 2023-08-05.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
